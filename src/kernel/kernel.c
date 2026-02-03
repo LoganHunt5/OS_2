c458962011a82aee2c100b39af8dae7ddebcf5a4
Logan Hunt <l.b.hunt5@gmail.com>
2026-02-02 18:25:12 -0700

    scroll when display is full

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -2,6 +2,9 @@
 #include "inc/kernel/kernel.h"
 void kernel_main(){
   vga_init(VGA_16_BLACK, VGA_16_WHITE);
-  vga_16_puts("Peed myself\0");
+  for(uint16_t i = 0; i < VGA_TEXT_HEIGHT*50; i ++){
+    if(i %2 == 0) vga_16_puts("Peed myself\n\0");
+    if(i %2 == 1) vga_16_puts("Pooped myself\n\0");
+  }
 }
 
--- src/kernel/vga/vga.c
+++ src/kernel/vga/vga.c
@@ -8,6 +8,7 @@ static uint16_t cursor_pos_x = 0;
 static uint16_t cursor_pos_y = 0;
 static void enable_cursor(uint8_t cursor_start, uint8_t cursor_end);
 static void update_cursor(uint16_t x, uint16_t y);
+static void scroll_line();
 
 void vga_init(enum vga_16_COLORS back,enum vga_16_COLORS fore) {
   fg = fore;
@@ -61,6 +62,17 @@ void vga_16_pch(char c){
   if(cursor_pos_x == VGA_TEXT_WIDTH || c == '\n'){
     cursor_pos_x = 0;
     cursor_pos_y++;
+    if(cursor_pos_y == VGA_TEXT_HEIGHT) scroll_line();
+  }
+}
+
+static void scroll_line(){
+  cursor_pos_y--;
+  update_cursor(cursor_pos_x, cursor_pos_y);
+  uint16_t* vga_text_pointer = (uint16_t*) VGA_TEXT_BUFFER;
+  uint16_t* vga_text_end = VGA_TEXT_WIDTH * 2 * (VGA_TEXT_HEIGHT-1) + vga_text_pointer;
+  for(uint16_t* i = vga_text_pointer; i < vga_text_end; i++){
+    *i = *(i+VGA_TEXT_WIDTH); 
   }
 }
 
@@ -69,6 +81,7 @@ void vga_16_puts(char* s){
   while(s[i] != '\0'){
     vga_16_pch(*(s + i));
     i++;
+    if(i > 128) return;
   }
   update_cursor(cursor_pos_x, cursor_pos_y);
 }
